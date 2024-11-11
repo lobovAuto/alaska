af75f65f53fb511402c167121ccb326acbc46482
Victor Lobov <1449812@mail.ru>
2024-11-11 13:18:39 +0000

    feeat: test to check container was completed

--- src/test_01_is_container.py
+++ src/test_01_is_container.py
@@ -10,4 +10,9 @@ def test_is_even():
 
 def test_is_container():
     client = docker.from_env()
-    client.containers.run("azshoo/alaska", "echo hello")
+    list_containers = client.images.list()
+    condition = False
+    for image in list_containers:
+        if image.tags[0] == "azshoo/alaska:1.0":
+            condition = True
+    assert condition == True
